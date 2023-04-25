441e95f107bb1b447f5826dc4b7b926c086b0832
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-04-25 09:20:22 -0300

    imprimindo resultado no htmk

--- index.php
+++ index.php
@@ -3,11 +3,22 @@ require_once 'classes/cashIn.php';
 require_once 'classes/cashOut.php';
 require_once 'data/currentAccounts.php';
 
-// cashOut(amount: 400, account: $currentAccounts['987.654.321-00']);
 
-// cashIn(amount: 200, depositorCpf: "123.456.789-00", depositedCpf: "987.654.321-00"); 
+$cashIn = new CashIn(amount: 100, depositorCpf: "123.456.789-00", depositedCpf: "987.654.321-00");
+$message = $cashIn->setBalanceDiff();
 
-$cashIn = new CashIn(amount: 10000, depositorCpf: "123.456.789-00", depositedCpf: "987.654.321-00");
-$cashIn->setBalanceDiff();
-var_dump($currentAccounts['123.456.789-00']);    
-var_dump($currentAccounts["987.654.321-00"]);     
+
+// echo "<p style='color: red; font-size: 64px; font-weight: bold;'>{$message} </p>"s;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP BANK</title>
+</head>
+<body>
+  <h1><?= $message  ?></h1>
+</body>
+</html>
